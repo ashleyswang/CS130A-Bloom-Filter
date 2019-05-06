0fdd0f715540c9cca1272bde9a0c31424214d92f
ashleyswang <38049558+ashleyswang@users.noreply.github.com>
2019-05-05 20:53:43 -0700

    Update BloomFilter.cpp

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -1,1 +1,21 @@
 #include "BloomFilter.h"
+#include "IntegerHashes.h"
+#include "StringHashes.h"
+#include <cstdint>
+#include <string>
+
+BloomFilter::BloomFilter(){
+  // code
+}
+
+BloomFilter::~BloomFilter(){
+  //code
+}
+
+void BloomFilter::insert(const std::string& value){
+  //code
+}
+
+bool BloomFilter::lookup(const std::string& value) const{
+  //code
+}
